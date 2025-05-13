b6fb2a26f728d4c9c7627f9be551906e50a399be
shyl <sohaylae615@gmail.com>
2025-05-13 21:46:30 +0300

    to stop random inputs

--- src/axios/countryData.ts
+++ src/axios/countryData.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const fetchCountry = async (name:string) : Promise<CountryResponse> => {
   try {
-      const response = await axios.get<CountryResponse[]>(`https://restcountries.com/v3.1/name/${name}`);
+      const response = await axios.get<CountryResponse[]>(`https://restcountries.com/v3.1/name/${name}?fullText=true`);
 
   return response.data[0];
   } catch (err) {
